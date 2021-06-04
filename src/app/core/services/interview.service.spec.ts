8cee9afdc43709d15dad407ed67bc0e97ba7a84e
Arthan-12 <43565516+Arthan-12@users.noreply.github.com>
2021-06-04 13:20:09 -0300

    service-test-w-injector-getAll-findId-put

--- src/app/core/services/interview.service.spec.ts
+++ src/app/core/services/interview.service.spec.ts
@@ -3,6 +3,8 @@ import { HttpClientTestingModule, HttpTestingController } from '@angular/common/
 import { of } from "rxjs";
 import { Interview } from "../models/interview.model";
 import { InterviewService } from "./interview.service";
+import { HttpResponse } from "@angular/common/http";
+import { url } from "inspector";
 
 
 describe(InterviewService.name, () => {
@@ -39,6 +41,36 @@ describe(InterviewService.name, () => {
         expect(req.request.method).toBe('GET');
         req.flush(expectedInterviews);
     });
+
+    it(`#${InterviewService.prototype.editInterview.name}
+    should update an interview and return it when called`, () => {
+        const expectedInterviewId: number = 1;
+        const expectedUpdatedInterview: Interview = { id: 1, category: 'Front End', vacancy: 'Desenvolvedor Angular Sr' };
+
+        interviewService.editInterview(expectedInterviewId, expectedUpdatedInterview).subscribe(
+            interview => {
+                expect(interview).toEqual(expectedUpdatedInterview)
+                //fail
+            }
+        );
+
+        const req = httpClientMock.expectOne( req => req.method === 'PUT' && req.url ===`${interviewService.API_URL}/${expectedInterviewId}`);
+        req.flush(expectedUpdatedInterview);
+    });
+
+    it(`#${InterviewService.prototype.findById.name}
+    should return a specific interviview by it's ID when called`, () => {
+        const expectedInterviewId: number = 1;
+        const expectedInterview: Interview = {id: 1, category: 'Front End', vacancy: 'Desenvolvedor Angular Pl'};
+
+        interviewService.findById(expectedInterviewId).subscribe(
+            interview => {
+                expect(interview).toEqual(expectedInterview)
+            }
+        );
+
+        const req = httpClientMock.expectOne(req => req.method === 'GET' && req.url === `${interviewService.API_URL}/${expectedInterviewId}`);
+        req.flush(expectedInterview);
+        
+    })
 });
-
-
